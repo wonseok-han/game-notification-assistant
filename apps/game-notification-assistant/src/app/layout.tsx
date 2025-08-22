5e84e47c4d889af114e60538d06d9b6c4ef59320
wonseok-han <oshan1112@gmail.com>
2025-08-22 12:44:24 +0900

    vercel analytics 적용

--- apps/game-notification-assistant/src/app/layout.tsx
+++ apps/game-notification-assistant/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 
 import { SnackbarManager } from '@repo/ui';
+import { Analytics } from '@vercel/analytics/next';
 import localFont from 'next/font/local';
 import './globals.css';
 
@@ -38,6 +39,7 @@ export default function RootLayout({
       >
         {children}
         <SnackbarManager maxSnackbars={5} />
+        <Analytics />
       </body>
     </html>
   );
